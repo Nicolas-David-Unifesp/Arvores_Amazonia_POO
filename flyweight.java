82634e5f9e29ce3458613a222849ab0e2944165a
Nicolas <nicolas.david@unifesp.br>
2026-05-19 22:21:13 -0300

    editado

--- flyweight.java
+++ flyweight.java
@@ -22,7 +22,7 @@ class ConcreteFlyweight implements flyweight {
 class MutableFlyweight implements flyweight {
     private String actual_height_tree;
     private String diameter_at_breast_height;
-    public void setActual_height_tree(String actual_height_tree, String diameter_at_breast_height) {
+    public MutableFlyweight(String actual_height_tree, String diameter_at_breast_height) {
         this.actual_height_tree = actual_height_tree;
         this.diameter_at_breast_height = diameter_at_breast_height;
     }
@@ -37,8 +37,23 @@ class FlyweightFactory {
     public flyweight getFlyweight(String key) {
         if (!flyweights.containsKey(key)) {
             String[] attributes = key.split(",");
-            flyweights.put(key, new ConcreteFlyweight(attributes[0], attributes[1], attributes[2]));
+            flyweights.put(key, new ConcreteFlyweight(attributes[0], attributes[1], attributes[2]), new MutableFlyweight());
         }
         return flyweights.get(key);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        FlyweightFactory factory = new FlyweightFactory();
+
+        flyweight tree1 = factory.getFlyweight("Oak,Rough,20m");
+        flyweight tree2 = factory.getFlyweight("Pine,Smooth,30m");
+
+        tree1.operation();
+        tree2.operation();
+
+        MutableFlyweight mutableTree = new MutableFlyweight("15m", "50cm");
+        mutableTree.operation();
     }
 }
